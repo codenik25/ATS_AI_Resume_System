bdcc777bcea56a4b4910c95599279f7fb901b87d
codenik25 <143266204+codenik25@users.noreply.github.com>
2026-01-27 14:07:34 +0530

    Adding ignore file

--- ml_engine/hybrid_ranker.py
+++
@@ -1,3 +0,0 @@
-def calculate_final_score(skill_score, semantic_score, skill_weight=0.4, semantic_weight=0.6):
-    final_score = (skill_score * skill_weight) + (semantic_score * semantic_weight)
-    return round(final_score, 2)
--- ml_engine/skill_gap.py
+++
@@ -1,3 +0,0 @@
-def find_skill_gap(resume_skills, job_skills):
-    missing_skills = set(job_skills) - set(resume_skills)
-    return list(missing_skills)
